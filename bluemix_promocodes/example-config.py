249f96e0e48a89c6d7977d9f6d9bb3fe96ffe325
Sebastian Schrader <sebastian.schrader@tu-dresden.de>
2015-11-06 14:15:33 +0100

    Create example configs

---
+++ bluemix_promocodes/example-config.py
@@ -0,0 +1,10 @@
+# Name of the SQL Database service in Bluemix/CloudFoundry
+SQLDB_SERVICE = 'sqldb-01'
+# Name of the Sendgrid service in Bluemix/CloudFoundry
+SENDGRID_SERVICE = 'sendgrid-c0'
+# A secret key for Flask
+SECRET_KEY = 'correcthorsebatterystaple'
+# The email address for outgoing mail
+EMAIL_ADDRESS = 'me@example.com'
+# Debug mode (never set this to True in production!)
+DEBUG = False
